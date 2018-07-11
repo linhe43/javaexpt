ec6c981e2aeb35f85b2d0d71487248bdfcd7ea53
linhe43@gmail.com <lhe@bioinfor.com>
2018-07-10 22:39:23 -0400

    Completed Percolation.java.

--- src/percolation/Percolation.java
+++ src/percolation/Percolation.java
@@ -1,17 +1,171 @@
 package percolation;
 
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+import java.util.Random;
+
 public class Percolation {
+
+    private int n;
+    private int head, tail;
+    private boolean[][] statusMat;
+    private int openSiteNum = 0;
+
+    private WeightedQuickUnionUF uf;
+
+    /**
+     * create n-by-n grid, with all sites blocked
+     * @param n
+     */
+    public Percolation(int n) {
+        this.n = n;
+        head = 0;
+        tail = n * n + 1;
+        statusMat = new boolean[n][n];
+        uf = new WeightedQuickUnionUF(tail + 1);
+    }
+
+    /**
+     * open site (row, col) if it is not open already
+     * @param row
+     * @param col
+     */
+    public void open(int row, int col) {
+        if (isOpen(row, col)) {
+            return;
+        }
+
+        statusMat[row - 1][col - 1] = true;
+        openSiteNum++;
+
+        int curIdx = posToIdx(row, col);
+        if (row == 1) {
+            uf.union(head, curIdx);
+        }
+        if (row == n) {
+            uf.union(curIdx, tail);
+        }
+
+        // check its neighbours for union;
+        if (row - 1 > 0) {
+            if (isOpen(row - 1, col)) {
+                uf.union(curIdx, posToIdx(row - 1, col));
+            }
+        }
+        if (row + 1 <= n) {
+            if (isOpen(row + 1, col)) {
+                uf.union(curIdx, posToIdx(row + 1, col));
+            }
+        }
+        if (col - 1 > 0) {
+            if (isOpen(row, col - 1)) {
+                uf.union(curIdx, posToIdx(row, col - 1));
+            }
+        }
+        if (col + 1 <= n) {
+            if (isOpen(row, col + 1)) {
+                uf.union(curIdx, posToIdx(row, col + 1));
+            }
+        }
+    }
+
+    /**
+     * is site (row, col) open?
+     * @param row
+     * @param col
+     * @return
+     */
+    public boolean isOpen(int row, int col) {
+        return statusMat[row - 1][col - 1];
+    }
+
+    /**
+     * is site (row, col) full
+     * @param row
+     * @param col
+     * @return
+     */
+    public boolean isFull(int row, int col) {
+        return uf.connected(head, posToIdx(row, col));
+    }
+
+    /**
+     * number of open sites
+     * @return
+     */
+    public int numberOfOpenSites() {
+        return openSiteNum;
+    }
+
+    /**
+     * does the system percolate?
+     * @return
+     */
+    public boolean percolates() {
+        return uf.connected(head, tail);
+    }
+
+    /**
+     * calculate the array index from (row, col)
+     * @param row
+     * @param col
+     * @return
+     */
+    private int posToIdx(int row, int col) {
+        return (row - 1) * n + col;
+    }
+
+    public boolean[][] getStatusMat() {
+        return statusMat;
+    }
+
+    public static void printMat(boolean[][] mat) {
+        int rNum = mat.length, cNum = mat[0].length;
+        for (int i = 0; i < rNum; i++) {
+            for (int j = 0; j < cNum; j++) {
+                if (mat[i][j]) {
+                    System.out.print('o');
+                } else {
+                    System.out.print('x');
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public final static void clearConsole() {
+        try {
+            final String os = System.getProperty("os.name");
+
+            if (os.contains("Windows")) {
+                Runtime.getRuntime().exec("cls");
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (final Exception e) {
+            //  Handle any exceptions.
+        }
+    }
+
     public static void main(String[] args) {
-        WeightedQuickUnionUF uf = new WeightedQuickUnionUF(5);
-        uf.union(0, 1);
-        uf.union(1, 2);
-        uf.union(3, 4);
-        System.out.println(uf.connected(0, 2));
-        System.out.println(uf.connected(2, 4));
-        System.out.println(uf.count() + " components");
+        int MAX_ITER_NUM = 10000;
+        int n = 20;
+        double avgFrac = 0;
+        int iterNum = 0;
+        while (iterNum++ < MAX_ITER_NUM) {
+            Percolation p = new Percolation(n);
+            Random rand = new Random(System.currentTimeMillis());
+            while (true) {
+                int row = rand.nextInt(n) + 1, col = rand.nextInt(n) + 1;
+                p.open(row, col);
+                if (p.percolates()) {
+                    avgFrac += (double) p.numberOfOpenSites() / (n * n);
+                    break;
+                }
+            }
+        }
+
+        System.out.println(avgFrac / MAX_ITER_NUM);
     }
 }
